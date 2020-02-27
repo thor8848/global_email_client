f30647b38bdadb5ca9192ca93dfbd509cfb50cde
thorDemo <40915413+thorDemo@users.noreply.github.com>
2020-02-27 20:57:32 +0800

    JACK 邮件服务 每小时15封 单账户 一次一封 返回验证过的账户

--- main.py
+++ main.py
@@ -14,43 +14,56 @@ log = Log('send_email.log').get_log()
 
 
 def get_email_mission():
-    response = requests.get('http://120.78.10.75:5004/email/', timeout=2)
+    response = requests.get('http://106.14.137.1:5004/email/', timeout=2)
     return response.json()
 
 
 def get_global_account():
-    response = requests.get('http://120.78.10.75:5004/account/')
+    response = requests.get('http://106.14.137.1:5004/account/')
     return response.json()
 
 
-def send_mail_mission():
-    ...
+def post_auth_user(u, p):
+    requests.get('http://127.0.0.1:5004/auth_account/?username={}&password={}'.format(u, p))
+    return
 
 
 if __name__ == '__main__':
     rand_time = random.randint(10, 20)
     log.warning('RANDOM WAIT {}'.format(rand_time))
-    time.sleep(rand_time)
+    # time.sleep(rand_time)
+    log.warning('CONNECT EMAIL SERVER')
+    while True:
+        account_data = get_global_account()
+        username = ''
+        password = ''
+        try:
+            username = account_data['username']
+            password = account_data['password']
+            smtp_host = 'smtp.global-mail.cn'
+            log.warning('ACCOUNT LOGIN TRY {} {}'.format(username, password))
+            server = SMTP_SSL(smtp_host)
+            # server.set_debuglevel(1)
+            server.ehlo(smtp_host)
+            server.login(username, password)
+            # 返回数据
+            post_auth_user(username, password)
+            break
+        except SMTPAuthenticationError:
+            log.warning('ACCOUNT FAILED {}{}'.format(username, password))
+            continue
+        except RequestException:
+            log.warning('CONNECT FAIL RETRY')
+            time.sleep(10)
+            continue
+        except KeyError:
+            log.warning('ACCOUNT EMPTY')
+            time.sleep(20)
+            continue
     while True:
         mission_data = dict()
         try:
-            log.warning('CONNECT EMAIL SERVER')
             mission_data = get_email_mission()
-            while True:
-                account_data = get_global_account()
-                username = account_data['username']
-                password = account_data['password']
-                try:
-                    smtp_host = 'smtp.global-mail.cn'
-                    log.warning('ACCOUNT LOGIN TRY {} {}'.format(username, password))
-                    server = SMTP_SSL(smtp_host)
-                    # server.set_debuglevel(1)
-                    server.ehlo(smtp_host)
-                    server.login(username, password)
-                    break
-                except SMTPAuthenticationError:
-                    log.warning('ACCOUNT FAILED {}{}'.format(username, password))
-                    continue
             message = MIMEText(mission_data['message'], _subtype='html', _charset='utf-8')
             message['Accept-Language'] = "zh-CN"
             message['Accept-Charset'] = "ISO-8859-1,UTF-8"
