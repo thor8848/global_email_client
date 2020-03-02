d621b1130e4c7e35ae3cc01edd1d7c6e20046cda
thorDemo <40915413+thorDemo@users.noreply.github.com>
2020-03-02 11:00:09 +0800

    JACK 邮件服务 每小时15封 单账户 一次7封 返回验证过的账户

--- main.py
+++ main.py
@@ -33,54 +33,58 @@ if __name__ == '__main__':
     log.warning('RANDOM WAIT {}'.format(rand_time))
     # time.sleep(rand_time)
     log.warning('CONNECT EMAIL SERVER')
-    try:
-        while True:
-            account_data = get_global_account()
-            username = ''
-            password = ''
-            try:
-                username = account_data['username']
-                password = account_data['password']
-                smtp_host = 'smtp.global-mail.cn'
-                log.warning('TTY LOGIN ACCOUNT {} {}'.format(username, password))
-                server = SMTP_SSL(smtp_host)
-                # server.set_debuglevel(1)
-                server.ehlo(smtp_host)
-                server.login(username, password)
-                # 返回数据
-                post_auth_user(username, password)
-                break
-            except SMTPAuthenticationError:
-                log.warning('ACCOUNT LOGIN FAILED {}{}'.format(username, password))
-                time.sleep(1)
-                continue
-            except RequestException:
-                log.warning('REQUEST FAILED RETRY')
-                time.sleep(10)
-                continue
-            except KeyError:
-                log.warning('USE OUT OF ACCOUNT')
-                time.sleep(20)
-                continue
-        while True:
-            mission_data = dict()
-            try:
-                mission_data = get_email_mission()
-                message = MIMEText(mission_data['message'], _subtype='html', _charset='utf-8')
-                message['Accept-Language'] = "zh-CN"
-                message['Accept-Charset'] = "ISO-8859-1,UTF-8"
-                message['From'] = encode_header(mission_data['from'], username)
-                message['To'] = encode_header(mission_data['to'], '')
-                message['Message-ID'] = uuid.uuid4().__str__()
-                message['Subject'] = Header(mission_data['subject'], 'utf-8')
-                message['MIME-Version'] = '1.0'
-                server.sendmail(username, ['thorhx@gmail.com', ], message.as_string())
-                log.warning('SEND SUCCESS EMAIL {}'.format(mission_data['receivers']))
-                log.warning('WAITING {} SEC'.format(mission_data['delay']))
-                time.sleep(int(mission_data['delay']))
-            except RequestException:
-                log.warning('REQUEST FAILED RETRY')
-                time.sleep(10)
-    except SMTPServerDisconnected:
-        time.sleep(10)
-        log.warning('Connection unexpectedly closed retry')
+    while True:
+        try:
+            while True:
+                account_data = get_global_account()
+                username = ''
+                password = ''
+                try:
+                    username = account_data['username']
+                    password = account_data['password']
+                    smtp_host = 'smtp.global-mail.cn'
+                    log.warning('TTY LOGIN ACCOUNT {} {}'.format(username, password))
+                    server = SMTP_SSL(smtp_host)
+                    # server.set_debuglevel(1)
+                    server.ehlo(smtp_host)
+                    server.login(username, password)
+                    # 返回数据
+                    post_auth_user(username, password)
+                    break
+                except SMTPAuthenticationError:
+                    log.warning('ACCOUNT LOGIN FAILED {}{}'.format(username, password))
+                    time.sleep(1)
+                    continue
+                except RequestException:
+                    log.warning('REQUEST FAILED RETRY')
+                    time.sleep(10)
+                    continue
+                except KeyError:
+                    log.warning('USE OUT OF ACCOUNT')
+                    time.sleep(20)
+                    continue
+                except SMTPServerDisconnected:
+                    time.sleep(10)
+                    log.warning('Connection unexpectedly closed retry')
+            while True:
+                mission_data = dict()
+                try:
+                    mission_data = get_email_mission()
+                    message = MIMEText(mission_data['message'], _subtype='html', _charset='utf-8')
+                    message['Accept-Language'] = "zh-CN"
+                    message['Accept-Charset'] = "ISO-8859-1,UTF-8"
+                    message['From'] = encode_header(mission_data['from'], username)
+                    message['To'] = encode_header(mission_data['to'], '')
+                    message['Message-ID'] = uuid.uuid4().__str__()
+                    message['Subject'] = Header(mission_data['subject'], 'utf-8')
+                    message['MIME-Version'] = '1.0'
+                    server.sendmail(username, ['thorhx@gmail.com', ], message.as_string())
+                    log.warning('SEND SUCCESS EMAIL {}'.format(mission_data['receivers']))
+                    log.warning('WAITING {} SEC'.format(mission_data['delay']))
+                    time.sleep(int(mission_data['delay']))
+                except RequestException:
+                    log.warning('REQUEST FAILED RETRY')
+                    time.sleep(10)
+        except SMTPServerDisconnected:
+            time.sleep(10)
+            log.warning('Connection unexpectedly closed retry')
